abf2fc03c3ab7cd92010f20738805609a73e2e2a
vladislava8sv <57909332+vladislava8sv@users.noreply.github.com>
2021-03-10 15:10:58 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+var str=n;
+str = parseFloat(str);//переводим строку в число, чтобы избавиться от -
+if (str<0) {
+   str=str*(-1);
+}
+var strReverse = str.toString().split('').reverse().join('');//переводим число в строку, чтобы воспользоваться методами
+return strReverse;
 }
